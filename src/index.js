3e239eec833718314176cdf44c9120e14c2d0689
Santiago Guerra <guerrasantiago19@gmail.com>
2020-07-13 15:34:53 -0500

    Initialize state with a simple object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './components/App';
+
+const initialState = {
+  books: [
+    {
+      title: 'Functional Programming',
+      author: 'Luis Atencio',
+      category: 'javascript',
+      id: Math.floor(Math.random * 1040),
+    },
+    {
+      title: 'Ruby by Example: Concepts and Code',
+      author: 'Kevin C. Baird',
+      category: 'ruby',
+      id: Math.floor(Math.random * 1040),
+    },
+    {
+      title: 'Haskell Programming from First Principles',
+      author: 'Chris Allen, Julie Moronuki',
+      category: 'haskell',
+      id: Math.floor(Math.random * 1040),
+    },
+    {
+      title: 'Get Programming with Go',
+      author: 'Nathan Youngman, Roger Peppé',
+      category: 'go',
+      id: Math.floor(Math.random * 1040),
+    },
+  ],
+};
 
 ReactDOM.render(
   <React.StrictMode>

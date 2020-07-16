c77d0d4f457f5a7108f49bef391a040e90de73fe
Santiago Guerra <guerrasantiago19@gmail.com>
2020-07-16 16:26:10 -0500

    Make the BooksForm component stateful and implement basic state: title, category and author

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,25 +1,36 @@
 import React from 'react';
 
-function BookForm() {
-  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+class BookForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      category: '',
+      author: '',
+    };
+  }
 
-  return (
-    <form>
-      <input name="title" placeholder="Title:" />
-      <input name="author" placeholder="Author:" />
-      <select name="category" id="category">
-        {categories.map(category => (
-          <option
-            value={category.toLowerCase()}
-            key={Math.floor(Math.random() * 1040)}
-          >
-            {category}
-          </option>
-        ))}
-      </select>
-      <button type="submit">CREATE</button>
-    </form>
-  );
+  render() {
+    const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+    return (
+      <form>
+        <input name="title" placeholder="Title:" />
+        <input name="author" placeholder="Author:" />
+        <select name="category" id="category">
+          {categories.map(category => (
+            <option
+              value={category.toLowerCase()}
+              key={Math.floor(Math.random() * 1040)}
+            >
+              {category}
+            </option>
+          ))}
+        </select>
+        <button type="submit">CREATE</button>
+      </form>
+    );
+  }
 }
 
 export default BookForm;

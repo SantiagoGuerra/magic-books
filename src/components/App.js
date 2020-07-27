673072691fc1d7a8738ed44c48a6e6fbefbbc6c2
Santiago Guerra <guerrasantiago19@gmail.com>
2020-07-27 16:43:16 -0500

    Implement header layout

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,25 @@ import BookForm from '../containers/BookForm';
 
 const App = () => (
   <div>
-    Filter:
+    <header className="header">
+      <h1 className="text-size-xl color-blue font-main">
+        Bookstore CMS
+      </h1>
+
+      <nav className="navigation">
+        <ul className="list-nav">
+          <li className="list-nav__item">
+            BOOKS
+          </li>
+
+          <li className="list-nav__item">
+            CATEGORIES
+          </li>
+        </ul>
+      </nav>
+
+      <div className="avatar" />
+    </header>
     <BookList />
     <BookForm />
   </div>

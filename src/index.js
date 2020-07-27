6d4489f61c8df3d6b89cf6d82929f26a0d3e71a1
Santiago Guerra <guerrasantiago19@gmail.com>
2020-07-27 16:22:12 -0500

    Set-up Sass

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import './styles/main.scss';
 import App from './components/App';
 import booksReducer from './reducers/books';
 import filterReducer from './reducers/filter';

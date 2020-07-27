9a50bd1d9e66daa1f6d73e12f74ffae4a58b6e87
Santiago Guerra <guerrasantiago19@gmail.com>
2020-07-27 17:10:37 -0500

    Implement container

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,25 @@ import BookForm from '../containers/BookForm';
 
 const App = () => (
   <div>
+
     <header className="header">
+      <div className="container">
+        <div className="header-wrap">
+          <h1 className="text-size-xl color-blue font-main logo">
+            Bookstore CMS
+          </h1>
+          <nav className="navigation">
+            <ul className="list-nav">
+              <li className="list-nav__item">
+                BOOKS
+              </li>
 
-      <div className="header-wrap">
-        <h1 className="text-size-xl color-blue font-main logo">
-          Bookstore CMS
-        </h1>
-        <nav className="navigation">
-          <ul className="list-nav">
-            <li className="list-nav__item">
-              BOOKS
-            </li>
-
-            <li className="list-nav__item">
-              CATEGORIES
-            </li>
-          </ul>
-        </nav>
+              <li className="list-nav__item">
+                CATEGORIES
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
 
       <div className="avatar" />
